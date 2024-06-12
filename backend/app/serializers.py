a4d9ddb982b572e9026692101080abeaebcd30e1
SleimaD <sleima@Ordinateur-portable-de-Sleima.local>
2024-06-12 14:49:03 +0200

    add of serializer for members

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -242,3 +242,9 @@ class OfferSerializer(serializers.ModelSerializer):
     class Meta:
         model = Offer
         fields = '__all__'
+
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        fields = '__all__'
