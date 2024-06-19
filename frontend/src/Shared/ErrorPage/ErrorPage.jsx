4f129c7077cf9df3ecf003a50d67f446ff8b5eb4
SleimaD <sleima@Ordinateur-portable-de-Sleima.local>
2024-06-19 11:21:05 +0200

    add of 404 page

--- frontend/src/Shared/ErrorPage/ErrorPage.jsx
+++ frontend/src/Shared/ErrorPage/ErrorPage.jsx
@@ -1,25 +1,37 @@
 import { Link } from "react-router-dom";
+import "./../../index.css"
 
 const ErrorPage = () => {
   return (
-    <main className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
-      <h1 className="text-8xl sm:text-9xl font-extrabold text-white tracking-widest">
-        404
-      </h1>
-      <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
-        Page Not Found
-      </div>
-      <button className="mt-5">
-        <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
-          <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
-
-          <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-            <Link to="/">Go Home</Link>
-          </span>
-        </a>
-      </button>
+    <main className="h-screen w-full flex flex-col justify-center items-center">
+      <section className="page_404 w-full">
+        <div className="container">
+          <div className="row">	
+          <div className="col-sm-12 ">
+          <div className="col-sm-10 col-sm-offset-1  text-center">
+          <div className="four_zero_four_bg">
+            <h1 className="text-center ">404</h1>
+          </div>
+          <div className="contant_box_404">
+          <h3 className="text-2xl">
+            Looks like you're lost 👀
+          </h3>
+          <p className="text-2xl">the page you are looking for not avaible!</p>
+          
+          <Link to="/" className="link_404 flex justify-center items-center mt-4 ">
+            <button className="btn-primary">
+              Go to Home
+            </button>
+          </Link>
+        </div>
+          </div>
+          </div>
+          </div>
+        </div>
+      </section>    
     </main>
   );
 };
 
 export default ErrorPage;
+
